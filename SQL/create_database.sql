e65adc4b04c232dad3305cfe6006a39d7f86a1dc
Chris <64387366+chris91003@users.noreply.github.com>
2022-05-14 00:09:03 -0700

    Add files via upload

---
+++ SQL/create_database.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE gene_info
+(GeneName VARCHAR (50),
+Gene_ID VARCHAR (25),
+Tax_ID  VARCHAR (25),
+Description VARCHAR (100));
+
+
+
+CREATE TABLE drug_info
+(Drug_Name VARCHAR (30),
+ Description VARCHAR (200)); 
